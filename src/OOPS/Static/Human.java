1cc2733dc1b955f8b0065996a46d906cbbe313b6
Lakshay Bhatia <coderlakshay289143@gmail.com>
2023-07-26 14:15:41 +0530

    Normal commit

--- src/OOPS/Static/Human.java
+++ src/OOPS/Static/Human.java
@@ -7,19 +7,14 @@ public class Human
     int salary;
     boolean married;
     static long population;
+    static String coutry;
 
-//    public Human(int age, String name, int salary, boolean married) {
-//        this.age = age;
-//        this.name = name;
-//        this.salary = salary;
-//        this.married = married;
-//    }
+
     public Human(int age , String name , int salary , boolean married){
         this.age = age;
         this.name = name;
         this.salary = salary;
         this.married = married;
-//        this.population += 1;
         population += 1;
 
     }
